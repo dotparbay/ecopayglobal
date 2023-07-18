aca77f8663e3245aa0f193cbeae891617e78d05e
dotparbay <137042164+dotparbay@users.noreply.github.com>
2023-07-18 07:38:54 +0000

    after build

---
+++ lib/provider.g.dart
@@ -0,0 +1,76 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$baseTabViewHash() => r'8838185acdfef36dfac4339e9c05421c7609a6f0';
+
+/// See also [BaseTabView].
+@ProviderFor(BaseTabView)
+final baseTabViewProvider =
+    AutoDisposeNotifierProvider<BaseTabView, ViewType>.internal(
+  BaseTabView.new,
+  name: r'baseTabViewProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$baseTabViewHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BaseTabView = AutoDisposeNotifier<ViewType>;
+String _$fetchSolBalanceUIHash() => r'd132665c68e6f69e65854aa7d48a944f7503024f';
+
+/// See also [FetchSolBalanceUI].
+@ProviderFor(FetchSolBalanceUI)
+final fetchSolBalanceUIProvider =
+    AutoDisposeAsyncNotifierProvider<FetchSolBalanceUI, String>.internal(
+  FetchSolBalanceUI.new,
+  name: r'fetchSolBalanceUIProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchSolBalanceUIHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FetchSolBalanceUI = AutoDisposeAsyncNotifier<String>;
+String _$fetchUSDTBalanceUIHash() =>
+    r'a1592f36105e560dfe95c1b1c579c059aca48bb9';
+
+/// See also [FetchUSDTBalanceUI].
+@ProviderFor(FetchUSDTBalanceUI)
+final fetchUSDTBalanceUIProvider =
+    AutoDisposeAsyncNotifierProvider<FetchUSDTBalanceUI, String>.internal(
+  FetchUSDTBalanceUI.new,
+  name: r'fetchUSDTBalanceUIProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchUSDTBalanceUIHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FetchUSDTBalanceUI = AutoDisposeAsyncNotifier<String>;
+String _$loadTransactionInfoListStreamUIHash() =>
+    r'579c0c8c1ebfe25fc8da3a4f88a939ea599293dc';
+
+/// See also [LoadTransactionInfoListStreamUI].
+@ProviderFor(LoadTransactionInfoListStreamUI)
+final loadTransactionInfoListStreamUIProvider =
+    AutoDisposeStreamNotifierProvider<LoadTransactionInfoListStreamUI,
+        List<TransactionInfo>>.internal(
+  LoadTransactionInfoListStreamUI.new,
+  name: r'loadTransactionInfoListStreamUIProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loadTransactionInfoListStreamUIHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoadTransactionInfoListStreamUI
+    = AutoDisposeStreamNotifier<List<TransactionInfo>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
